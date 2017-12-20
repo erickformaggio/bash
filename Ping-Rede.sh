1f0673896ab9a11e3ba21f2a3f0cd8e8395d37b7
erickformaggio <erick.beltrami@gmail.com>
2017-12-20 21:49:37 -0200

    exit code

--- Ping-Rede.sh
+++ Ping-Rede.sh
@@ -7,10 +7,12 @@
 DATA=$(date)
 if [ $1 == '']
 then
-	echo "Use ./Ping-Rede.sh [sua rede]"
-	echo "Exemplo ./Ping-Rede.sh 192.168.20"
+	echo "-Use ./Ping-Rede.sh [sua rede]"
+	echo "-Exemplo ./Ping-Rede.sh 192.168.20"
+	exit 1
 else
 	for x in `seq 1 254`; do
 		ping -c 1 $1.$x | grep "64 bytes" # | cut -d" " -f4 | sed 's/.$//'
 	done
+	exit 0
 fi	
