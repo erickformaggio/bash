104fdc104e5c4f45182b68cd9a80e4ded56c2e82
erickformaggio <erick.beltrami@gmail.com>
2017-12-24 08:52:26 -0200

    calculo de data no final

--- Ping-Rede.sh
+++ Ping-Rede.sh
@@ -4,15 +4,34 @@
 # Erick Formaggio - erick.beltrami@gmail.com
 # Data 06/12/17
 
+# Variáveis
 DATA=$(date)
-if [ $1 == '']
+REDE=$(ifconfig | grep "inet 1") 
+MINI=$(date +%M)
+
+if [ "$#" -eq 0 ]
 then
-	echo "-Use ./Ping-Rede.sh [sua rede]"
-	echo "-Exemplo ./Ping-Rede.sh 192.168.20"
+	echo "-------------------------------------"
+	echo "- Use ./Ping-Rede.sh [sua rede]     -"
+	echo "- Exemplo ./Ping-Rede.sh 192.168.20 -"
+	echo "-------------------------------------"
+	echo ""
+	echo "- Abaixo, talvez tenhamos informações sobre seu ip"
+	echo $REDE
 	exit 1
 else
-	for x in `seq 1 254`; do
-		ping -c 1 $1.$x | grep "64 bytes" # | cut -d" " -f4 | sed 's/.$//'
+	echo ""
+	echo "Iniciando em $DATA"
+	echo ""
+	echo "Rastreando... O processo pode levar alguns minutos!"
+	for x in `seq 1 254`; do 
+		ping -c 1 $1.$x | grep "64 bytes" #| cut -d" " -f4 | sed 's/.$//'
 	done
-	exit 0
-fi	
+
+	MFIM=$(date +%M)
+	TEMPO=$(($MFIM-$MINI))
+	echo ""
+	echo "Tempo de execução $TEMPO minuto(s)"
+
+	exit 1
+fi
